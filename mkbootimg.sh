6d885d5c769dc3fbe85ccbf1896bf00708e37677
catro <catro@qq.com>
2012-09-21 15:21:47 +0800

    Rename flashable zip to update.zip.

--- mkbootimg.sh
+++ mkbootimg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ -e bootstrap/boot.img ] && rm bootstrap/boot.img
-[ -e kernel.zip ] && rm kernel.zip
+[ -e update.zip ] && rm update.zip
 
 pushd bootstrap/ramdisk/
 find . | cpio -o -H newc | gzip > ../device/ramdisk.cpio.gz
@@ -18,6 +18,6 @@ popd
 tools/mkbootimg --kernel bootstrap/zImage --ramdisk bootstrap/bootstrap.cpio.gz --cmdline "console=ttySAC2,115200 consoleblank=0" --base 0x10000000 --pagesize 4096 --output bootstrap/boot.img
 
 pushd bootstrap
-zip -r ../kernel.zip boot.img META-INF/
+zip -r ../update.zip boot.img META-INF/
 popd
 exit 0
